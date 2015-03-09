774dcfb57b2e1deeb325d90264210d60c2b73b0b
Pranav Kulkarni <pranavcode@gmail.com>
2015-03-09 13:08:22 +0530

    Some style changes.

--- header.php
+++ header.php
@@ -1,5 +1,7 @@
 <link href="style.css" rel="stylesheet" type="text/css">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Berkshire+Swash' rel='stylesheet' type='text/css'>
 <?php
-echo '<p style="margin: 0 auto; padding: 10px;"><a style="color: #09648b;; font: 35px monospace, courier bold; padding: 10px;" href="index.php">insilico</a>';
+echo '<p style="margin: 0 auto; padding: 10px;"><a style="color: #09648b; font-family: \'Berkshire Swash\'; font-size: 35px; padding: 10px;" href="index.php">insilico</a>';
 echo 'Neuronal Simulation Library</p>';
 ?>
