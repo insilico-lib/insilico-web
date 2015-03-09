b02a2c8e014f33312cee2d790802320854524a64
Pranav Kulkarni <pranavcode@gmail.com>
2015-03-09 12:17:42 +0530

    Style fix for button.

--- download.php
+++ download.php
@@ -1,9 +1,7 @@
 <?php include_once("pre_wrapper.php"); ?>
 
 <p class="page-content-header">Download</p>
-
-<p style="text-align: center; padding: 10px;"><a href="insilico-0.17.zip"><button style="font-size: 35px; border-radius: 20px;">&downarrow; insilico 0.17</button></a></p>
-
+<p style="text-align: center; padding: 10px;"><a href="insilico-0.17.zip"><span class="btn">insilico 0.17</span></a></p>
 <p style="padding-left: 10px;">
     <span>Current stable release:</span><b> Version 0.17 </a></b> (February 11, 2015)
 </p>
@@ -20,13 +18,13 @@
 <p style="padding-left: 10px;">
     <span>Download links to previous versions:</span>
     <ul>
-    <li><a href="insilico-0.16.zip">Version 0.16</a> (January 21, 2015)</li>	
-    <li><a href="insilico-0.15.zip">Version 0.15</a> (January 19, 2015)</li>
-    <li><a href="insilico-0.10.zip">Version 0.10</a> (January 3, 2015)</li>
-    <li><a href="insilico-0.04.zip">Version 0.04</a> (December 17, 2014)</li>
-    <li><a href="insilico-0.035.zip">Version 0.035</a> (December 6, 2014)</li>
-    <li><a href="insilico-0.03.zip">Version 0.03</a> (December 4, 2014)</li>
-    <li><a href="insilico-0.02.zip">Version 0.02</a> (December 2, 2014)</li>
+	<li><a href="insilico-0.16.zip">Version 0.16</a> (January 21, 2015)</li>
+	<li><a href="insilico-0.15.zip">Version 0.15</a> (January 19, 2015)</li>
+	<li><a href="insilico-0.10.zip">Version 0.10</a> (January 3, 2015)</li>
+	<li><a href="insilico-0.04.zip">Version 0.04</a> (December 17, 2014)</li>
+	<li><a href="insilico-0.035.zip">Version 0.035</a> (December 6, 2014)</li>
+	<li><a href="insilico-0.03.zip">Version 0.03</a> (December 4, 2014)</li>
+	<li><a href="insilico-0.02.zip">Version 0.02</a> (December 2, 2014)</li>
     </ul>    
 </p>    
 <?php include_once("post_wrapper.php"); ?>
--- menu.php
+++ menu.php
@@ -1,8 +1,8 @@
 <?php
   $menu = array("Documentation", "Download");
-  echo '<ul style="border-radius: 10px; margin: 0px; list-style-type: none; padding: 10px; background: #3388ac;">';
+  echo '<ul class="menu">';
   foreach($menu as $menuitem) { 
-  echo '<li style="padding: 10px; display: inline;">'.
+  echo '<li class="menu-list">'.
        '<a style="color: #fff;" href='.strtolower($menuitem).'.php>'.
       $menuitem.
        '</a></li>';
