a4c57bcf035b76d6e336fc83fbfb3e5fd615ef0c
Pranav Kulkarni <pranavcode@gmail.com>
2015-03-09 17:03:24 +0530

    Version 0.20 released.

--- download.php
+++ download.php
@@ -1,23 +1,25 @@
 <?php include_once("pre_wrapper.php"); ?>
 
 <p class="page-content-header">Download</p>
-<p style="text-align: center; padding: 10px;"><a href="insilico-0.17.zip"><span class="btn">insilico 0.17</span></a></p>
+<p style="text-align: center; padding: 10px;"><a href="insilico-0.20.zip"><span class="btn">insilico 0.20</span></a></p>
 <p style="padding-left: 10px;">
-    <span>Current stable release:</span><b> Version 0.17 </a></b> (February 11, 2015)
+    <span>Current stable release:</span><b> Version 0.20 </a></b> (March 9, 2015)
 </p>
 <p style="padding-left: 10px;">
     <span>Significant updates in current stable release:</span>
     <ul>
-	<li>Observation of constants from Neurons <small style="color:green;">(new feature)</small></li>
-	<li>Improved compilation time <small style="color:green;">(new)</small></li>
-	<li>Explicit include directory <small style="color:green;">(new)</small></li>
-	<li>Values transferred from connections (synapses) to nodes (neurons) are double precision floating points <small style="color:red;">(bug fix)</small></li>
-	<li>Connectivity file (a.k.a. synapse file) is optional <small style="color:red;">(bug fix)</small></li>
+	<li>Injecting Extrenal Current through input file <small style="color:green;">(new feature)</small></li>
+	<li>User interface improved <small style="color:green;">(new)</small></li>
+	<li>Cross platform support <small style="color:green;">(new)</small></li>
+	<li>Updated local doc <small style="color:green;">(new)</small></li>
+	<li>Examples Updated <small style="color:green;">(new)</small></li>
+	<li>Bug fixes and minor improvements <small style="color:red;">(bug fix)</small></li>
     </ul>
 </p>
 <p style="padding-left: 10px;">
     <span>Download links to previous versions:</span>
     <ul>
+	<li><a href="insilico-0.17.zip">Version 0.17</a> (February 11, 2015)</li>
 	<li><a href="insilico-0.16.zip">Version 0.16</a> (January 21, 2015)</li>
 	<li><a href="insilico-0.15.zip">Version 0.15</a> (January 19, 2015)</li>
 	<li><a href="insilico-0.10.zip">Version 0.10</a> (January 3, 2015)</li>
