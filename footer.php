73a88551e54bd7a9529cba3e0084de6a624d2042
Pranav Kulkarni <pranavcode@gmail.com>
2015-03-18 19:06:32 +0530

    Style and footer

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <?php
-  echo '<p style="font-size: 10px;border-top: 1px solid #2B2B2B; margin: 0 auto; padding: 10px">Copyright &copy; 2015 Collins Assisi</p>';
+  echo '<p style="font-size: 10px;border-top: 1px solid #2B2B2B; margin: 0 auto; padding: 10px">Copyright &copy; 2015 Dr Collins Assisi, Theoretical Neuroscience Lab, IISER Pune (Last Updated: March 18, 2015)</p>';
 ?>
--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <link href="style.css" rel="stylesheet" type="text/css">
-<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Berkshire+Swash' rel='stylesheet' type='text/css'>
 <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
 <script src="script.js"></script>
