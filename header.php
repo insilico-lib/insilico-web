fd2478009e3d75eb5e21f0acd0009b31ca081529
Pranav Kulkarni <pranavcode@gmail.com>
2015-04-27 01:42:29 +0530

    Resolve conflicts.

--- header.php
+++ header.php
@@ -1,16 +1,10 @@
 <link href="style.css" rel="stylesheet" type="text/css">
-<<<<<<< HEAD
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:300'
       rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Berkshire+Swash'
       rel='stylesheet' type='text/css'>
 <script src="http://code.jquery.com/jquery-latest.min.js"
 	type="text/javascript"></script>
-=======
-<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Berkshire+Swash' rel='stylesheet' type='text/css'>
-<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
->>>>>>> parent of 73a8855... Style and footer
 <script src="script.js"></script>
 <p style="margin: 0 auto; padding: 10px;">
     <a style="color: #09648b; font-family: 'Open Sans'; font-size: 35px; padding: 10px;" href="index.php">
