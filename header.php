efccb747eaaa49f9c43f72c079c800b960791bcf
Pranav Kulkarni <pranavcode@gmail.com>
2015-04-27 01:34:50 +0530

    Style change.

--- header.php
+++ header.php
@@ -1,9 +1,14 @@
 <link href="style.css" rel="stylesheet" type="text/css">
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Berkshire+Swash' rel='stylesheet' type='text/css'>
-<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300'
+      rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Berkshire+Swash'
+      rel='stylesheet' type='text/css'>
+<script src="http://code.jquery.com/jquery-latest.min.js"
+	type="text/javascript"></script>
 <script src="script.js"></script>
-<?php
-echo '<p style="margin: 0 auto; padding: 10px;"><a style="color: #09648b; font-family: \'Berkshire Swash\'; font-size: 35px; padding: 10px;" href="index.php">insilico</a>';
-echo 'Neuronal Simulation Library</p>';
-?>
+<p style="margin: 0 auto; padding: 10px;">
+    <a style="color: #09648b; font-family: 'Open Sans'; font-size: 35px; padding: 10px;" href="index.php">
+        insilico
+    </a>
+    Neuronal Simulation Library
+</p>
--- wrapper.php
+++ wrapper.php
@@ -4,4 +4,6 @@ include_once("doctype_decl.php");
 echo '<div class="wrapper">';
 include_once("header.php");
 include_once("menu.php");
+include_once("footer.php");
+echo "</div>"
 ?>
